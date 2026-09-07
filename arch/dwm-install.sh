3c2c685d60743c37c3e5555319e70b9edc399ade
rabbi-lion <marebrk@icloud.com>
2026-09-07 16:07:28 +0200

    Update dwm-install.sh

--- arch/dwm-install.sh
+++ arch/dwm-install.sh
@@ -22,7 +22,7 @@ DOTFILES_REPO="https://github.com/rabbi-lion/dotfiles.git"
 GTK_THEME_REPO="https://github.com/edwinagudelojaramillo/retro-theme.git"
 ICON_THEME_REPO="https://www.opencode.net/deepspacewhine/indigo-reality.git"
 
-DEFAULT_WALLPAPER="aero-pharaoh.png"
+DEFAULT_WALLPAPER="space.jpg"
 
 
 # ------------------------------------------------------------
